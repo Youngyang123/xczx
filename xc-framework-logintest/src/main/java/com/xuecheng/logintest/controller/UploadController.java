2b82b505ee39ea2f0413b9cf460613dd15e945ce
ysm <youngysm@163.com>
2019-11-25 19:56:01 +0800

    dfd

--- xc-framework-logintest/src/main/java/com/xuecheng/logintest/controller/UploadController.java
+++ xc-framework-logintest/src/main/java/com/xuecheng/logintest/controller/UploadController.java
@@ -3,12 +3,15 @@ package com.xuecheng.logintest.controller;
 import com.xuecheng.framework.model.response.ObjectResponse;
 import com.xuecheng.logintest.utils.FtpUtil;
 import com.xuecheng.logintest.utils.IDUtils;
+import org.apache.commons.net.ftp.FTPClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("images")
@@ -24,5 +27,21 @@ public class UploadController {
                 "123456", "/home/ftpuser", "/images", fileName, file.getInputStream());
         String imageUrl = "http://" + host + port + "/images/" + fileName;
         return new ObjectResponse<>(20000, "上传成功", imageUrl);
+
+
+
+    }
+
+    public void test() throws IOException {
+        FTPClient ftp = new FTPClient();
+        //设置 ip 和端口,写在用户名和密码上面
+        ftp.connect("192.168.139.131", 21);
+        //设置用户名和密码
+        ftp.login("ftpuser", "ftpuser");
+        //设置文件类型
+        ftp.setFileType(FTPClient.BINARY_FILE_TYPE);
+        InputStream is = new FileInputStream("E:/test.jpg");
+        //第一个参数存储时名称
+        ftp.storeFile("test.jpg", is);
     }
 }
